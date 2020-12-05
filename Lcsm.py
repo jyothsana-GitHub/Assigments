840952be8ee7f1ef4c0d3d2be082dd47a71d3568
Jyothsana_10 <72886244+jyothsana-GitHub@users.noreply.github.com>
2020-12-05 22:53:30 +0100

    Lscm
    Lcsm

---
+++ Lcsm.py
@@ -0,0 +1,26 @@
+from Bio import SeqIO
+
+input_file = 'rosalind_lcsm.txt'
+
+with open(input_file) as f:
+    fasta_sequences = list(SeqIO.parse(f, 'fasta'))
+    sequences = [str(fasta.seq) for fasta in fasta_sequences]
+
+def long_substr(data):
+    substr = ''
+    if len(data) > 1 and data[0]:
+        for i in range(len(data[0])):
+            for j in range(len(data[0]) - i + 1):
+                if j > len(substr) and is_substr(data[0][i: i + j], data):
+                    substr = data[0][i: i + j]
+    return substr
+
+def is_substr(find, data):
+    if len(data) < 1 and len(find) < 1:
+        return False
+    for dat in data:
+        if find not in dat:
+            return False
+    return True
+
+print(long_substr(sequences))
